76a19eb586df58805a3900b35ca9d57b0b1574c9
Mohamed Hamdi <haamdi@outlook.com>
2023-06-21 07:25:51 +0100

    Font on system

--- modules/system-services.nix
+++ modules/system-services.nix
@@ -1,4 +1,83 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let
+  variants = {
+    design = {
+      tilde = "low";
+      asterisk = "penta-low";
+      underscore = "low";
+      at = "fourfold";
+      zero = "dotted";
+      dollar = "open";
+      percent = "dots";
+      four = "semi-open-non-crossing";
+    };
+  };
+  weights = {
+    extralight = {
+      shape = 200;
+      menu = 200;
+      css = 200;
+    };
+    regular = {
+      shape = 400;
+      menu = 400;
+      css = 400;
+    };
+    bold = {
+      shape = 700;
+      menu = 700;
+      css = 700;
+    };
+  };
+  slopes = {
+    upright = {
+      angle = 0;
+      shape = "upright";
+      menu = "upright";
+      css = "normal";
+    };
+
+    italic = {
+      angle = 9.4;
+      shape = "italic";
+      menu = "italic";
+      css = "italic";
+    };
+  };
+  buildIosevka = pkgs.iosevka.override;
+  iosevka-ss08 = buildIosevka {
+    set = "ss08";
+    privateBuildPlan = {
+      family = "Iosevka";
+
+      inherit variants weights slopes;
+    };
+  };
+  iosevka-ss08-term = buildIosevka {
+    set = "ss08-term";
+    privateBuildPlan = {
+      family = "Iosevka Term";
+      spacing = "term";
+
+      inherit variants weights slopes;
+    };
+  };
+  nf-patch = font:
+    pkgs.stdenvNoCC.mkDerivation {
+      pname = "${font.pname}-nerd-font-patched";
+      version = font.version;
+
+      src = font;
+
+      nativeBuildInputs = [ pkgs.nerd-font-patcher ];
+
+      buildPhase = ''
+        mkdir -p $out
+        find -name \*.ttf -exec nerd-font-patcher -o $out/share/fonts/truetype/ -c {} \;
+      '';
+      installPhase = "";
+    };
+in {
   boot.plymouth.enable = true;
   programs.dconf.enable = true;
   console.keyMap = "fr";
@@ -65,6 +144,8 @@
   };
   fonts.fonts = with pkgs; [
     # Fonts
+    (nf-patch iosevka-ss08-term)
+    iosevka-ss08
     vistafonts
     jetbrains-mono
     helvetica-neue-lt-std
