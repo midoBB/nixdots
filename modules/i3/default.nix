bc413b628a187a9a57e7c5deb89eaf0e7aaf5280
Mohamed Hamdi <haamdi@outlook.com>
2024-02-26 16:00:48 +0100

    lun. 26 févr. 2024 16:00:48 CET

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -1,3 +1,9 @@
 {
-  xdg.configFile."i3/config".source = ./config;
+  xdg = {
+    configFile = {
+      "i3/config".source = ./config;
+      "i3/workspace1.json".source = ./workspace1.json;
+      "i3/workspace7.json".source = ./workspace7.json;
+    };
+  };
 }
